d62b3f43a2669246e119db3cb837d40fd0685619
sarabmadsen <71260111+sarabmadsen@users.noreply.github.com>
2021-01-26 13:58:39 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,22 @@ Count to 7 and update span.count with the value of the count.
 
 Once 7 is reached stop the count and enable the button.*/
 
+const button = document.querySelector("button");
+const countContainer = document.querySelector(".count");
+let counter = 0;
+
+button.onclick = function() {
+    event.target.disabled = true;
+
+    counter = 0;
+
+    const intervalId = setInterval(function () {
+        counter ++;
+        countContainer.innerHTML = counter;
+
+        if(counter === 7) {
+            clearInterval(intervalId);
+            button.disabled = false;
+        }
+    },1000);
+};
